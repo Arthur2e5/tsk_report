a6846da0f4f0bb9985f8d3258c7eb8471cd24dbf
Incubator85653 <2bemperor47@gmail.com>
2016-05-29 01:46:07 +0800

    Completed server part of AHS
    Reload Ruby environment to "ruby-1.9.3-p551-i386-mingw32" by default at
    the same time.

--- tsk_report_v004_International(CN_UnArchive)/bin/ocra.bat
+++ tsk_report_v004_International(CN_UnArchive)/bin/ocra.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"ruby.exe" "C:/Program Files (custom)/Ruby193-tsk/bin/ocra" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"ruby.exe" "C:/Users/GL121/Documents/GitHub/tsk_report/tsk_report_v004_International(CN_UnArchive)/rubyEnv/ruby-1.9.3-p551-i386-mingw32/bin/ocra" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
 @"ruby.exe" "%~dpn0" %*
--- tsk_report_v004_International(CN_UnArchive)/lib/ruby/gems/1.9.1/gems/openssl-0.2.0/lib/openssl.rb
+++
@@ -1,7 +0,0 @@
-require "openssl/version"
-
-puts "Error! You've somehow installed openssl from rubygems.org"
-puts "Remove openssl from your Gemfile and delete it from your gems"
-
-# module Openssl
-# end
